e46c4b9241341403117d5c9fdb6e3c21ffbd5696
Ubuntu <ubuntu@ip-10-236-169-62.ec2.internal>
2014-01-06 01:54:28 +0000

    added vendor directory to gitignore

---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$i = 0;
+
+$app = function ($request, $response) use (&$i) {
+    $i++;
+    $pid = getmypid();
+
+    $text = "This is request number $i for #$pid.\n";
+    $headers = array('Content-Type' => 'text/plain');
+
+    $response->writeHead(200, $headers);
+    $response->end($text);
+};
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($socket);
+
+$http->on('request', $app);
+
+$socket->listen(1337);
+
+// lets try multiple children
+for ($counter = 0; $counter < 5; $counter++) { 
+	$pid = pcntl_fork();
+
+	if ($pid == -1) {
+	     die('could not fork');
+	} else if ($pid) {
+	     // we are the parent
+	     //pcntl_wait($status); //Protect against Zombie children
+
+		//$loop->run();
+	} else {
+	     // we are the child
+		$loop->run();
+	}
+}
