9bed4fd7ad4a3af8eed27c3a4917f12e9de998d1
Ubuntu <ubuntu@ip-10-110-51-123.ec2.internal>
2014-01-06 12:58:50 +0000

    added configuration class

---
+++ lib/PUnicorn/Configuration.php
@@ -0,0 +1,13 @@
+<?php /** @author Christian Calloway callowaylc@gmail */
+namespace PUnicorn;
+
+/** Represents punicorn server configuration */
+class Configuration {
+
+	function __static() {
+
+	}
+}
+
+// @TODO move to autoloader?
+Configuration::__static();
--- lib/PUnicorn/Server.php
+++ lib/PUnicorn/Server.php
@@ -6,4 +6,14 @@ namespace PUnicorn;
  **/
 class Server { 
 
+	function __static() {
+		$loop   = React\EventLoop\Factory::create();
+		$socket = new React\Socket\Server($loop);
+		$http   = new React\Http\Server($socket);		
+	}
 }
+
+// I dont really like doing this; but the choice is to do an explicit
+// call on __static or modify composer autoloader, which would present
+// a whole lotta fun fucking times
+Server::__static();
