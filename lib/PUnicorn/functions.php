678e6d5f653acada9a9f73e9f685b88ccb5f2a8b
Christian Calloway <callowaylc@gmail.com>
2014-01-07 15:40:23 +0000

    fleshed out definition of PUnicorn.throws

--- lib/PUnicorn/functions.php
+++ lib/PUnicorn/functions.php
@@ -1,6 +1,19 @@
 <?php /** Christian Calloway callowaylc@gmail.com */
 namespace PUnicorn;
 
-function throws() { 
-	// @PASS
+/** Shortcut for throwing exception */
+function throws($exception = null) {
+
+	// @TODO we need to do tighter checks here to
+	// determine exception type; if null, this is usually
+	// a @PASS for debugging sake and we determine file + line #
+	if (is_null($exception)) {
+		$trace     = debug_backtrace()[0];
+		$exception = "{$trace['file']} @ #{$trace['line']}";
+	}
+
+	// if exception is string, wrap in standard exception
+	is_string($exception) && $exception = new \Exception($exception);
+
+	throw $exception;
 }
